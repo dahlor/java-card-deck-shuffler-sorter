9eb2c49f6a01f6bccc46e3c7e4352855566cb66c
Eric Dahl <dahlor@Orion.local>
2021-06-18 01:46:17 -0400

    Printlines in and Card class created.

---
+++ src/main/java/com/atlantiscoding/Card.java
@@ -0,0 +1,5 @@
+package com.atlantiscoding;
+
+public class Card {
+		
+}
---
+++ src/main/java/com/atlantiscoding/ShufflerSorter.java
@@ -0,0 +1,21 @@
+package com.atlantiscoding;
+
+public class ShufflerSorter {
+	
+	public static void main(String[] args) {
+		
+		System.out.println( "" + " cards in the deck.");
+		
+		System.out.println("\nShuffled Deck:");
+		
+		System.out.println("\nSorted by Rank:");
+
+		System.out.println("\nSorted by Suit and Rank:");
+
+		
+		
+		
+		
+		
+	}	
+}
