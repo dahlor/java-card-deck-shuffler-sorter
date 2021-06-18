1542d2c9c5b40039828ce5f16d55af1066abe9c4
Eric Dahl <dahlor@Orion.local>
2021-06-18 03:05:48 -0400

    Added rank and suit constructor / toString.

--- src/main/java/com/atlantiscoding/Card.java
+++ src/main/java/com/atlantiscoding/Card.java
@@ -1,5 +1,21 @@
 package com.atlantiscoding;
 
 public class Card {
+	
+	private int rank;
+    private int suit;
+    
+    public Card(int rank, int suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    
+    public String toString() {
+        String[] ranks = {null, null, "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", 
+        				  "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING", "ACE"};
+        String[] suits = {"HEARTS", "DIAMONDS", "SPADES", "CLUBS"};
+        String cardString = ranks[this.rank] + " of " + suits[this.suit];
+        return cardString;
+    }
 		
 }
--- src/main/java/com/atlantiscoding/ShufflerSorter.java
+++ src/main/java/com/atlantiscoding/ShufflerSorter.java
@@ -8,18 +8,18 @@ public class ShufflerSorter {
 	public static void main(String[] args) {
 		
 		List<Card> deckOfCards = new ArrayList<Card>();
+
+		
+		
+// Now that we set up and shuffled the deck, let's print it all out:		
 		
 		System.out.println( "" + " cards in the deck.");
 		
 		System.out.println("\nShuffled Deck:");
-		
+
 		System.out.println("\nSorted by Rank:");
 
 		System.out.println("\nSorted by Suit and Rank:");
-
-		
-		
-		
 		
 		
 	}	
