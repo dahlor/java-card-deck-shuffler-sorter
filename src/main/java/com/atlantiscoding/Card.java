082e84fc29857cda67c98f344fb8293caf1cc87f
Eric Dahl <dahlor@Orion.local>
2021-06-18 03:08:13 -0400

    Formatting.

--- src/main/java/com/atlantiscoding/Card.java
+++ src/main/java/com/atlantiscoding/Card.java
@@ -3,7 +3,7 @@ package com.atlantiscoding;
 public class Card {
 	
 	private int rank;
-    private int suit;
+	private int suit;
     
     public Card(int rank, int suit) {
         this.rank = rank;
@@ -11,8 +11,7 @@ public class Card {
     }
     
     public String toString() {
-        String[] ranks = {null, null, "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", 
-        				  "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING", "ACE"};
+        String[] ranks = {null, null, "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING", "ACE"};
         String[] suits = {"HEARTS", "DIAMONDS", "SPADES", "CLUBS"};
         String cardString = ranks[this.rank] + " of " + suits[this.suit];
         return cardString;
